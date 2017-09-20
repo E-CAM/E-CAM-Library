fd65d756a0f59085f43da5c5fa71bf5a01e2814d
Alan O'Cais <a.ocais@fz-juelich.de>
2017-09-20 12:07:34 +0200

    Revert to pngmath until we have everything working

--- conf.py
+++ conf.py
@@ -35,7 +35,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx.ext.imgmath',
+    'sphinx.ext.pngmath',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
 ]
