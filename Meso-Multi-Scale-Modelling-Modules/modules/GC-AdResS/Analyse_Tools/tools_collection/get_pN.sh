e8e7ff9d9124180e0a1a74b85beba90bb1422723
Alan O'Cais <a.ocais@fz-juelich.de>
2019-05-14 12:49:23 +0200

    Remove tools directory and rename tarball to what appears in readme

--- Meso-Multi-Scale-Modelling-Modules/modules/GC-AdResS/Analyse_Tools/tools_collection/get_pN.sh
+++
@@ -1,9 +0,0 @@
-vmd -dispdev text -e extract_coord.tcl
-grep -B1 "Frame" WCG.xyz > a
-sed '/Frame/ {$!N;d;}' a > column2
-grep -B0 "Frame" WCG.xyz > a
-sed -i s/Frame// a
-sed -i s/--// a
-sed -i s/:// a
-sed '/^$/d' a > column1
-paste column1 column2|awk '{print $1, $2}' > dat.3nm.pn.WCG.dat
